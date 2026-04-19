74d3ae99c0494b3c41c8054b5f6c82e2fb067ccb
houssem-gasmi <houssem.gasmi41@gmail.com>
2026-04-19 18:06:36 +0100

    Handle missing Render env vars

--- public/index.php
+++ public/index.php
@@ -5,12 +5,15 @@ use Symfony\Component\ErrorHandler\Debug;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
-if ($_SERVER['APP_DEBUG'] ?? false) {
+$_env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV') ?: 'prod';
+$_debug = filter_var($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: '0', FILTER_VALIDATE_BOOL);
+
+if ($_debug) {
     umask(0000);
     Debug::enable();
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) ($_SERVER['APP_DEBUG'] ?? false));
+$kernel = new Kernel($_env, $_debug);
 $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
