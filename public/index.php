7530043447c98ce62752e1fd999074087d77c242
houssem-gasmi <houssem.gasmi41@gmail.com>
2026-04-19 18:40:50 +0100

    Fix mailer DSN empty string crash on Render

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Symfony\Component\ErrorHandler\Debug;
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? getenv('APP_ENV') ?: 'prod';
-$_debug = filter_var($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: '1', FILTER_VALIDATE_BOOL);
+$_debug = filter_var($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? getenv('APP_DEBUG') ?: '0', FILTER_VALIDATE_BOOL);
 
 if ($_debug) {
     umask(0000);
